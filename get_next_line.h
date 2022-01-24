cf9c7e2d0ef5d13d3bca897ae7036d642085b96e
Elliot Galibert <elliotgalibert@Elliot-MacBook-Pro.local>
2022-01-24 15:52:07 +0200

    all

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elliotgalibert <elliotgalibert@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/24 15:33:04 by elliotgalib       #+#    #+#             */
+/*   Updated: 2022/01/24 15:33:27 by elliotgalib      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFF_SIZE 8
+# define MAX_FD 1024 + 1
+# define RET_VALUE(ret)	ret > 0 ? 1 : ret
+
+int		get_next_line(int const fd, char **line);
+
+#endif
